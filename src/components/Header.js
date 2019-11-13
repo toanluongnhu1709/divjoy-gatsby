0dc8ce5a502a32e953b289ff6a2fcc13fc0061db
dharmveer97 <dvbangar8@gmail.com>
2019-11-13 23:23:28 +0530

    delete extra components & add navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,40 +2,13 @@
 
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'gatsby';
 
-const Section = styled.section`
-  padding: 1rem 1.5rem;
-  font-family: ${props => props.theme.primaryFontFamily};
-  .navbar {
-    background-color: transparent;
-  }
-  .navbar-brand {
-    margin-right: 20px;
-    .navbar-item img {
-      max-height: 3.75rem;
-    }
-  }
-  .navbar-menu {
-    @media screen and (max-width: 600px) {
-      position: absolute;
-      width: 100%;
-      transition: 0.6s;
-    }
-  }
-  .navbar-item {
-    font-weight: 700;
-    font-size: 1.2rem;
-    :hover {
-      color: ${props => props.theme.darkAccent};
-    }
-  }
-  .navbar-burger {
-    background-color: #1c1323;
-    color: #fff;
-    opacity: 0.6;
-    border-radius: 4px;
-  }
+const Section = styled.div`
+
+img {
+  width="112"
+  height="28"
+}
 `;
 
 export default class Header extends React.Component {
@@ -56,51 +29,37 @@ export default class Header extends React.Component {
   }
 
   render() {
-    const { isActive } = this.state;
-
     return (
       <Section className="section">
         <div className="container">
           <nav
-            className="navbar"
+            className="navbar is-white"
             role="navigation"
             aria-label="main navigation">
             <div className="navbar-brand">
-              <Link className="navbar-item" to="/">
-                <img src="/images/logo-1024.png" alt="site logo" />
-              </Link>
+              <a className="navbar-item" href="">
+                <img src="./images/logo-1024.svg" alt="mainlogo" />
+              </a>
+
               <a
-                href="#"
                 role="button"
-                className={
-                  isActive
-                    ? 'navbar-burger burger mobile is-active'
-                    : 'navbar-burger burger mobile'
-                }
+                className="navbar-burger burger"
                 aria-label="menu"
                 aria-expanded="false"
-                data-target="navbarBasicExample"
-                onClick={() => this.handleMobileMenu()}
-              >
+                data-target="navbarBasicExample">
                 <span aria-hidden="true" />
                 <span aria-hidden="true" />
                 <span aria-hidden="true" />
               </a>
             </div>
-            <div className={isActive ? 'navbar-menu is-active' : 'navbar-menu'}>
-              <div className="navbar-start">
-                <Link to="/" className="navbar-item">
-                  Home
-                </Link>
-                <Link to="/about" className="navbar-item">
-                  About
-                </Link>
-                <Link to="/news" className="navbar-item">
-                  News
-                </Link>
-                <Link to="/contact" className="navbar-item">
-                  Contact
-                </Link>
+
+            <div id="navbarBasicExample" className="navbar-menu">
+              <div className="navbar-end">
+                <div className="navbar-item">
+                  <div className="buttons">
+                    <a className="button is-light">Sign in</a>
+                  </div>
+                </div>
               </div>
             </div>
           </nav>
